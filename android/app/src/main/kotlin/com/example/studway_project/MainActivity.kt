23b0f71cf36f4eb96987f078aa5350df81a769aa
AyoubBF <ayoub.benfraj013@gmail.com>
2022-01-18 15:20:32 +0100

    dependencies

---
+++ android/app/src/main/kotlin/com/example/studway_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.studway_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Container(
+      color: Colors.red,
+      child: Center(
+        child: Text(
+          'Hello World',
+          textDirection: TextDirection.ltr,
+        ),
+      )));
+}
