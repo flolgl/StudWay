45206caf6af07b20870bad6ec602fbd6b2f2ffc0
flowdevLOL <48724807+flowdevLOL@users.noreply.github.com>
2022-01-26 18:34:21 +0100

    Input components avec Style

---
+++ lib/login/components/PasswordInput.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+
+class PasswordInput extends StatelessWidget {
+  final String text;
+
+  const PasswordInput({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO : Utiliser le thème pour les couleurs
+    return TextField(
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        hintText: text,
+        hintStyle: const TextStyle(color: Colors.grey),
+        filled: true,
+        fillColor: Colors.grey.shade200,
+        focusedBorder:const OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0xff4f6d9c), width: 2.0),
+        ),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey, width: 0.0),
+        ),
+
+      ),
+      obscureText: true,
+      enableSuggestions: false,
+      autocorrect: false,
+      cursorColor: const Color(0xff4f6d9c),
+
+
+    );
+  }
+}
