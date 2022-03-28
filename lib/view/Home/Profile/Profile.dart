85b0f956f125ee9addff97586057b4666f9f5ef2
abenfraj <72936702+abenfraj@users.noreply.github.com>
2022-03-28 17:41:33 +0200

    upload image from gallery or take direct photo with camera, have to test on a simulator or phone

--- lib/view/Home/Profile/Profile.dart
+++ lib/view/Home/Profile/Profile.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:studway_project/controller/user/User.dart';
 import 'package:studway_project/view/Home/Profile/pages/ExperienceForm.dart';
 import 'package:studway_project/view/Home/Profile/pages/FormationForm.dart';
@@ -8,10 +12,19 @@ import 'package:studway_project/view/Home/Profile/pages/FormationForm.dart';
 import '../../AppTheme.dart';
 import 'pages/CompetenceForm.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
+  final User user;
+  const Profile(this.user, {Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ProfileState(user);
+}
+
+class _ProfileState extends State<Profile> {
+  File? image;
   final User _user;
 
-  const Profile(this._user, {Key? key}) : super(key: key);
+  _ProfileState(this._user);
 
   @override
   Widget build(BuildContext context) {
@@ -22,43 +35,57 @@ class Profile extends StatelessWidget {
       ]),
     );
     return Container(
-      width: double.infinity,
-      decoration: gradientDecoration,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          const SizedBox(
-            height: 30,
-          ),
-          Center(
-            child: _buildCircleAvatar(),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
+        width: double.infinity,
+        decoration: gradientDecoration,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+        const SizedBox(
+        height: 30,
+        ),
+        Center(
+          child: _buildCircleAvatar(),
+        ),
+        Center(
+          child: buildButton(
+              title: 'Choisir depuis la gallerie',
+              icon: Icons.image_outlined,
+              onClicked: () => pickImage(ImageSource.gallery),
+        )),
+            Center(
+                child: buildButton(
+                  title: 'Prendre une photo',
+                  icon: Icons.image_outlined,
+                  onClicked: () => pickImage(ImageSource.gallery),
+                )),
+    const SizedBox(
+    height: 30,
+    ),
 
-          // Le container de la partie Options (container avec bg white)
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-              ),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    _buildStatsInfo(context),
-                    Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: _buildOptionsBody(context),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+    // Le container de la partie Options (container avec bg white)
+    Expanded(
+    child: Container(
+    width: double.infinity,
+    decoration: const BoxDecoration(
+    color: Colors.white,
+    ),
+    child: SingleChildScrollView(
+    child: Column(
+    children: [
+    _buildStatsInfo(context),
+    Padding(
+    padding: const EdgeInsets.all(15.0),
+    child: _buildOptionsBody(context),
+    ),
+    ],
+    ),
+    ),
+    ),
+    ),
+    ]
+    ,
+    )
+    ,
     );
 
     // return Center(
@@ -68,7 +95,10 @@ class Profile extends StatelessWidget {
 
   /// Méthode retournant le form de connexion
   Widget _buildOptionsBody(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
+    double width = MediaQuery
+        .of(context)
+        .size
+        .width;
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
@@ -326,26 +356,27 @@ class Profile extends StatelessWidget {
   void _buildErrorPopUp(BuildContext context) {
     showDialog<String>(
       context: context,
-      builder: (BuildContext context) => AlertDialog(
-        title: const Text('Impossible sur navigateur internet'),
-        actions: <Widget>[
-          Center(
-            child: TextButton(
-              onPressed: () => Navigator.pop(context, 'OK'),
-              child: const Text('OK'),
-            ),
+      builder: (BuildContext context) =>
+          AlertDialog(
+            title: const Text('Impossible sur navigateur internet'),
+            actions: <Widget>[
+              Center(
+                child: TextButton(
+                  onPressed: () => Navigator.pop(context, 'OK'),
+                  child: const Text('OK'),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
     );
   }
 
   // TODO : vraiment mettre la photo de profile
   /// Retournant un [CircleAvatar] du user
   Widget _buildCircleAvatar() {
-    return const CircleAvatar(
-      radius: 55,
-      backgroundImage: NetworkImage("http://localhost:3000/users/photoProfile/3"),
+    return CircleAvatar(
+      radius: 45,
+      backgroundImage: NetworkImage(_user.profilpic),
     );
   }
 
@@ -444,7 +475,41 @@ class Profile extends StatelessWidget {
   void _navigateToProfileForm(BuildContext context) {
     ;
   }
+
+  Future pickImage(ImageSource source) async {
+    try {
+      final image = await ImagePicker.pickImage(source: ImageSource.camera);
+      final imageTemporary = File(image.path);
+      setState(() => this.image = imageTemporary);
+    } on PlatformException catch (e) {
+      throw Exception(e.toString());
+    }
+  }
 }
+
+Widget buildButton({
+  required String title,
+  required IconData icon,
+  required VoidCallback onClicked,
+}) =>
+    Container(
+      margin: const EdgeInsets.only(top: 20.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            fixedSize: const Size(240, 35),
+            primary: Colors.white,
+            onPrimary: Colors.black,
+            textStyle: const TextStyle(fontSize: 15)),
+        child: Row(
+          children: [
+            Icon(icon, size: 28),
+            const SizedBox(width: 16),
+            Text(title),
+          ],
+        ),
+        onPressed: onClicked,
+      ),
+    );
 
 //
 // Container(
--- lib/view/offers/components/OfferForm.dart
+++ lib/view/offers/components/OfferForm.dart
@@ -11,7 +11,7 @@ class OfferForm extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: AddOfferBody(),
+      body: const AddOfferBody(),
     );
   }
 
