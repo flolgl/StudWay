a8d19eb7f44369344721a9d43d85e4be78a03d42
flowdevLOL <48724807+flowdevLOL@users.noreply.github.com>
2022-03-29 17:05:21 +0200

    user fav annonces list

--- lib/view/Home/Business/CandidatFavList.dart
+++ lib/view/Home/Business/CandidatFavList.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:studway_project/controller/offer/Offer.dart';
+import 'package:studway_project/controller/user/User.dart';
 
 class CandidatFavList extends StatefulWidget {
   const CandidatFavList({Key? key}) : super(key: key);
@@ -8,10 +10,79 @@ class CandidatFavList extends StatefulWidget {
 }
 
 class _FavListState extends State<CandidatFavList> {
+  late final List<Offer> _favList;
+  bool _isFetched = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getFavList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('FavList'),
+    if(!_isFetched){
+      print("ici");
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if(_favList.isEmpty){
+      return const Center(
+        child: Text("Aucune offre favorie"),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: _favList.length,
+      itemBuilder: (context, index) {
+        return Card(
+          child: ListTile(
+            title: Text(_favList[index].titre),
+            subtitle: Text(_favList[index].description),
+            leading: const Icon(Icons.favorite),
+            trailing: IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                deleteFav(_favList[index].id);
+              },
+            ),
+          ),
+        );
+      },
+
     );
   }
+
+  void getFavList() async {
+    var favList = await User.currentUser!.fetchCandidatFav();
+
+    setState(() {
+      _favList = favList;
+      _isFetched = true;
+    });
+  }
+
+  void deleteFav(int id) async {
+    var deleted = await User.currentUser!.deleteFav(id);
+    if (!deleted){
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }else{
+      setState(() {
+        _favList.removeWhere((element) => element.id == id);
+      });
+    }
+
+  }
+
+  final snackBar = SnackBar(
+    content: const Text('Erreur lors de la suppression'),
+    action: SnackBarAction(
+      label: 'Ok',
+      onPressed: () {
+        // Some code to undo the change.
+      },
+    ),
+  );
 }
